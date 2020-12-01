e6ffdca60410847399c94ba407a9e05b77467c8a
gabiralta <69698939+gabiralta@users.noreply.github.com>
2020-12-01 11:04:52 -0600

    mobile version

---
+++ js/dynamic-resolution.js
@@ -0,0 +1,18 @@
+function adjustStyle(width) {
+    width = parseInt(width);
+    if (width < 701) {
+        $("#size-stylesheet").attr("href", "css/narrow.css");
+    } else if ((width >= 701) && (width < 900)) {
+        $("#size-stylesheet").attr("href", "css/style.css");
+    } else {
+       $("#size-stylesheet").attr("href", "css/style.css"); 
+    }
+}
+
+$(function() {
+    adjustStyle($(this).width());
+    $(window).resize(function() {
+        adjustStyle($(this).width());
+    });
+});
+
