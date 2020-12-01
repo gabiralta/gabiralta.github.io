0786dc620c7ae5f8439195dbb0db8c1e09faa5a6
gabiralta <69698939+gabiralta@users.noreply.github.com>
2020-12-01 15:39:45 -0600

    change braquet space

--- js/dynamic-resolution.js
+++ js/dynamic-resolution.js
@@ -1,9 +1,9 @@
 function adjustStyle(width) {
     width = parseInt(width);
-    if (width < 400) {
+    if (width < 700) {
         $("#size-stylesheet").attr("href", "css/narrow.css");
-    } else if ((width >= 401) && (width < 900)) {
-        $("#size-stylesheet").attr("href", "css/style.css");
+    } else if ((width >= 701) && (width < 900)) {
+        $("#size-stylesheet").attr("href", "css/narrow.css");
     } else {
        $("#size-stylesheet").attr("href", "css/style.css"); 
     }
